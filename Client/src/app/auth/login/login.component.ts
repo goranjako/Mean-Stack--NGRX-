dfe95d14caa57fd39e5616517cea662b0282ed96
Goran Jakovljević <goranjako@gmail.com>
2022-10-15 10:23:59 +0200

    login

--- Client/src/app/auth/login/login.component.ts
+++ Client/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +12,33 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  registerForm= new FormGroup({
+    email: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+      Validators.maxLength(25)
+    ])),
+    password: new FormControl('',  Validators.compose([
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(25)
+    ]))
+  });
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+
+
+onSubmit(f:any) {
+
+console.log(f)
+}
 
 }
