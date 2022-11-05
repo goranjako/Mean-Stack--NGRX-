c5d4a88ca883a538207a5236500dcab67b5dd189
Goran Jakovljević <goranjako@gmail.com>
2022-11-05 20:57:05 +0100

    ngx

--- Client/src/app/contacts/contacts.module.ts
+++ Client/src/app/contacts/contacts.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { ContactsComponent } from './contacts.component';
     CommonModule,
     ContactsRoutingModule,
     FlexLayoutModule,
-    MaterialModule
+    MaterialModule,
+    NgxSpinnerModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ]
 })
 export class ContactsModule { }
